45a595550eb823f78c3780738e321d2dc007cdee
burnttomato <86201494+burnttomato@users.noreply.github.com>
2021-07-17 20:12:31 -0700

    Update small_voc_label_071721.py

--- scripts/small_voc_label_071721.py
+++ scripts/small_voc_label_071721.py
@@ -51,10 +51,14 @@ for year, image_set in sets:
         os.makedirs('VOCdevkit/VOC%s/labels/'%(year))
     image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
     list_file = open('%s_%s.txt'%(year, image_set), 'w')
-    for image_id in image_ids:
+    
+    #for image_id in image_ids:
+    for (image_id < 10) in image_ids:
+   
         list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(wd, year, image_id))
         convert_annotation(year, image_id)
-    list_file.close()
+        
+        list_file.close()
 
 #os.system("cat 2007_train.txt 2007_val.txt 2012_train.txt 2012_val.txt > train.txt")
 #os.system("cat 2007_train.txt 2007_val.txt 2007_test.txt 2012_train.txt 2012_val.txt > train.all.txt")
